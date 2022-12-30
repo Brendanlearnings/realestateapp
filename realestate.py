d240fbefddab9764cc680b01fe5697a895c00cc5
Brendanlearnings <brendanlearnings@gmail.com>
2022-12-30 15:32:11 +0200

    Add title

--- realestate.py
+++ realestate.py
@@ -1,6 +1,10 @@
 import streamlit as st 
 import pandas as pd 
 
+main_title = 'Real Estate Process Manager'
+
+st.title(main_title)
+
 df = pd.DataFrame({
   'first column': [1, 2, 3, 4],
   'second column': [10, 20, 30, 40]
